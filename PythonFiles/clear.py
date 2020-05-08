f9416e0408eb19627e2234c0fac61134fd9d8792
pps6 <shahparth1017@gmail.com>
2020-05-08 15:41:51 +0530

    clear file added

---
+++ PythonFiles/clear.py
@@ -0,0 +1,157 @@
+def new_rm_delete_clear_button(self):
+    self.uiWindow.new_rm_delete_product_code.clear()
+    self.uiWindow.new_rm_delete_product_name.clear()
+    self.uiWindow.new_rm_delete_product_price.clear()
+
+def new_rm_entry_clear_button(self):
+    self.uiWindow.new_rm_add_product_code.clear()
+    self.uiWindow.new_rm_add_product_name.clear()
+    self.uiWindow.new_rm_add_product_price.clear()
+
+def new_rm_modify_clear_button(self):
+    self.uiWindow.new_rm_modify_product_code.clear()
+    self.uiWindow.new_rm_modify_product_name.clear()
+    self.uiWindow.new_rm_modify_product_price.clear()
+
+def new_shade_delete_clear_button(self):
+    self.uiWindow.shade_new__delete_number.clear()
+    self.uiWindow.shade_new_delete_details_table.clearcontent()
+
+
+def new_shade_entry_clear_button(self):
+    self.uiWindow.shade_new_number.clear()
+    self.uiWindow.shade_new_details_table.clearcontent()
+
+
+def new_shade_modify_clear_button(self):
+    self.uiWindow.shade_new__modify_number.clear()
+    self.uiWindow.shade_new_modify_details_table.clearcontent()
+
+def new_shade_view_clear_button(self):
+    self.uiWindow.shade_new__view_number.clear()
+    self.uiWindow.shade_new_view_details_table.clearcontent()
+
+def rm_add_clear_button(self):
+    self.uiWindow.rm_customer.clear()
+    self.uiWindow.rw_remark.clear()
+    self.uiWindow.rm_addtable.clearcontent()
+
+
+def rm_delete_clear_button(self):
+    self.uiWindow.rw_delete_transaction_id.clear()
+    self.uiWindow.rm_delete_date.clear()
+    self.uiWindow.rm_delete_customer.clear()
+    self.uiWindow.rm_delete_remark.clear()
+    self.uiWindow.rm_delete_table.clearcontent()
+
+def rm_modify_clear_button(self):
+    self.uiWindow.rw_modify_transaction_id.clear()
+    self.uiWindow.rm_modify_date.clear()
+    self.uiWindow.rm_modify_customer.clear()
+    self.uiWindow.rm_modify_remark.clear()
+    self.uiWindow.rm_view_table.clearcontent()
+
+def rm_stock_view(self):
+    self.uiWindow.rw_view_stock_code.clear()
+    self.uiWindow.rw_view_stock_date.clear()
+    self.uiWindow.rw_view_stock_name.clear()
+    self.uiWindow.rm_view_table.clearcontent()
+    self.uiWindow.rw_view_closing.clear()
+
+
+def rm_view_by_id_clear_button(self):
+    self.uiWindow.rw_view_transaction_id.clear()
+    self.uiWindow.rm_view_date.clear()
+    self.uiWindow.rm_view_customer.clear()
+    self.uiWindow.rm_view_remark.clear()
+    self.uiWindow.rm_view_table.clearcontent()
+
+
+def sales_add_clear_button(self):
+    self.uiWindow.sales_add_customer.clear()
+    self.uiWindow.sales_add_remark.clear()
+    self.uiWindow.sales_add_table.clearcontent()
+
+def sales_delete_clear_button(self):
+    self.uiWindow.sales_delete_trans_id.clear()
+    self.uiWindow.sales_delete_date.clear()
+    self.uiWindow.sales_delete_customer.clear()
+    self.uiWindow.sales_delete_remark.clear()
+    self.uiWindow.sales_delete_table.clearcontent()
+
+
+def sales_modify_clear_button(self):
+    self.uiWindow.sales_modify_transid.clear()
+    self.uiWindow.sales_modify_date.clear()
+    self.uiWindow.sales_modify_customer.clear()
+    self.uiWindow.sales_modify_remark.clear()
+    self.uiWindow.sales_modify_table.clearcontent()
+
+def sales_view_by_id_clear_button(self):
+    self.uiWindow.sales_view_by_id_transaction_id.clear()
+    self.uiWindow.sales_view_by_id_date.clear()
+    self.uiWindow.sales_view_by_id_customer.clear()
+    self.uiWindow.sales_view_by_id_remark.clear()
+    self.uiWindow.sales_view_by_id_table.clearcontent()
+
+
+def shade_add_clear_button(self):
+    self.uiWindow.shade_number_add.clear()
+    self.uiWindow.shade_customer.clear()
+    self.uiWindow.shade_remark.clear()
+    self.uiWindow.shade_add_total.clear()
+    self.uiWindow.shade_addtable.clearcontent()
+    self.uiWindow.shade_colortable.clearcontent()
+
+
+def shade_delete_clear_button(self):
+    self.uiWindow.shade_delete_transaction_id.clear()
+    self.uiWindow.shade_delete_date.clear()
+    self.uiWindow.shade_delete_customer.clear()
+    self.uiWindow.shade_delete_remark.clear()
+    self.uiWindow.shade_number_delete.clear()
+    self.uiWindow.shade_delete_total.clear()
+    self.uiWindow.shade_delete_table.clearcontent()
+    self.uiWindow.shade_delete_colortable.clearcontent()
+
+
+def shade_modify_clear_button(self):
+    self.uiWindow.shade_modify_transaction_id.clear()
+    self.uiWindow.shade_modify_customer.clear()
+    self.uiWindow.shade_modify_date.clear()
+    self.uiWindow.shade_modify_remark.clear()
+    self.uiWindow.shade_number_modify.clear()
+    self.uiWindow.shade_modify_total.clear()
+    self.uiWindow.shade_modify_table.clearcontent()
+    self.uiWindow.shade_modify_colortable.clearcontent()
+
+
+def shade_stock_view_clear_button(self):
+    self.uiWindow.shade_view_stock_shade_number.clear()
+    self.uiWindow.shade_view_stock_code.clear()
+    self.uiWindow.shade_view_stock_date.clear()
+    self.uiWindow.shade_view_stock_name.clear()
+    self.uiWindow.shade_view_starting_date.clear()
+    self.uiWindow.shade_view_ending_date.clear()
+    self.uiWindow.shade_view_closing.clear()
+    self.uiWindow.shade_view_table.clearcontent()
+
+
+def shade_sales_clear_button(self):
+    self.uiWindow.shade_transaction_id.clear()
+    self.uiWindow.shade_customer.clear()
+    self.uiWindow.shade_remark.clear()
+    self.uiWindow.shade_sales_total.clear()
+    self.uiWindow.shade_sales_date.clear()
+    self.uiWindow.shade_sales_table.clearcontent()
+
+
+def shade_view_by_id_clear_button(self):
+    self.uiWindow.shade_view_transaction_id.clear()
+    self.uiWindow.shade_date.clear()
+    self.uiWindow.shade_view_customer.clear()
+    self.uiWindow.shade_view_remark.clear()
+    self.uiWindow.shade_number_add_view.clear()
+    self.uiWindow.shade_viewbyid_add_total.clear()
+    self.uiWindow.shade_view_by_id_colortable.clearcontent()
+    self.uiWindow.shade_view_by_id_table.clearcontent()
